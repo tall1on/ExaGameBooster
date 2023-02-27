86f62ad708e98f95b0ae19226398357645e93175
Luke Grandke <theplayer443@gmail.com>
2023-02-27 22:59:41 +0100

    Update install.bat

--- bat/install.bat
+++ bat/install.bat
@@ -36,5 +36,6 @@ mkdir "C:\Program Files\Exatek\ExaGameBooster"
 
 echo F | xcopy /S /Q /Y /F "%~dp0\ExaGameBooster.exe" "C:\Program Files\Exatek\ExaGameBooster\ExaGameBooster.exe*"
 del /f "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\CPUAffinityGameBooster.exe"
+del /f "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\ExaGameBooster.exe"
 mklink "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\ExaGameBooster.exe" "C:\Program Files\Exatek\ExaGameBooster\ExaGameBooster.exe"
 start /d "C:\Program Files\Exatek\ExaGameBooster" ExaGameBooster.exe
